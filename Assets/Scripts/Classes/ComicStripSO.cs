b0bf65ba1335978af5f5d3f7212adb9024afb9b4
JKDevs <cookseyjude@gmail.com>
2024-10-14 20:51:56 +0200

    Added the next scene variable to the TransitionDataSO

--- Assets/Scripts/Classes/ComicStripSO.cs
+++ Assets/Scripts/Classes/ComicStripSO.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "ComicStrip", menuName = "ScriptableObjects/Comics/ComicStrip", order = 1)]
 public class ComicStripSO : ScriptableObject
 {
     public int playComicIndex;
+    public string nextScene;
+
+    //Checks and Sets the nextScene string
+    public void SetScene(string scene)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (scene == SceneManager.GetSceneAt(i).name)
+            {
+                nextScene = scene;
+                return;
+            }
+        }
+    }
 }
